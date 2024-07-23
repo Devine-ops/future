de66cd92cc2791bd348fefa5b271a6f59e6a8f99
Devine-ops <vinigabrielsoa@gmail.com>
2024-07-23 08:14:23 -0300

    :tada: first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header (){
+    return (
+        <div class="container">
+            <nav>
+                <ul><li><a>Home</a></li></ul>
+                <ul><li><a>Home</a></li></ul>
+                <ul><li><a>Home</a></li></ul>
+                <ul><li><a>Home</a></li></ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
